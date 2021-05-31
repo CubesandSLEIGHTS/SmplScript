983bbda2a13e470ab4c8a507f01d0964e9ee646e
Stephen Steyaert <>
2021-05-31 03:41:22 +0000

    Fixing indentation errors and configured replit run button

--- SmplScript.py
+++ SmplScript.py
@@ -205,45 +205,45 @@ class Parser:
         self.tokens = tokens
         self.tok_idx = -1
         self.advance()
-    
-    def advance(self):
-        self.tok_idx +=1
-        if self.tok_idx < len(self.tokens): 
-            self.current_tok = self.tokens[self.tok_idx]
-        return self.current_tok
-    
-    def parse(self):
-        res = self.expr()
-        if not res.error and self.current_tok.type != TT_EOF:
-            return res.failure(InvalidSyntaxError(self.current_tok.pos_start, self.current_tok.pos_end, "Expected '+', '-', '*' or '/'"))
-        return res
-
-    def factor(self):
-        res = ParseResult()
-        tok = self.current_tok
-
-        if tok.type in (TT_PLUS, TT_MINUS):
-            res.register(self.advance())
-            factor = res.register(self.factor())
-            if res.error: return res
-            return res.success(UnaryOpNode(tok, factor))
-        
-        elif tok.type in (TT_INT, TT_FLOAT):
-            res.register(self.advance())
-            return res.success(NumberNode(tok))
-        
-        elif tok.type == TT_LPAREN:
-			res.register(self.advance())
-			expr = res.register(self.expr())
-			if res.error: return res
-			if self.current_tok.type == TT_RPAREN:
-				res.register(self.advance())
-				return res.success(expr)
-			else:
-				return res.failure(InvalidSyntaxError(
-					self.current_tok.pos_start, self.current_tok.pos_end,
-					"Expected ')'"
-				))
+		
+		def advance(self):
+				self.tok_idx += 1
+				if self.tok_idx < len(self.tokens):
+						self.current_tok = self.tokens[self.tok_idx]
+				return self.current_tok
+		
+		def parse(self):
+				res = self.expr()
+				if not res.error and self.current_tok.type != TT_EOF:
+					return res.failure(InvalidSyntaxError(self.current_tok.pos_start, self.current_tok.pos_end, "Expected '+', '-', '*' or '/'"))
+				return res
+		
+		def factor(self):
+				res = ParseResult()
+				tok = self.current_tok
+				
+				if tok.type in (TT_PLUS, TT_MINUS):
+						res.register(self.advance())
+						factor = res.register(self.factor())
+						if res.error: return res
+						return res.success(UnaryOpNode(tok, factor))
+				
+				elif tok.type in (TT_INT, TT_FLOAT):
+					res.register(self.advance())
+					return res.success(NumberNode(tok))
+				
+				elif tok.type == TT_LPAREN:
+					res.register(self.advance())
+					expr = res.register(self.expr())
+					if res.error: return res
+					if self.current_tok.type == TT_RPAREN:
+						res.register(self.advance())
+						return res.success(expr)
+					else:
+						return res.failure(InvalidSyntaxError(
+							self.current_tok.pos_start, self.current_tok.pos_end,
+							"Expected ')'"
+						))
         
         return res.failure(InvalidSyntaxError(tok.pos_start, tok.pos_end, "Expected int or float"))
     
